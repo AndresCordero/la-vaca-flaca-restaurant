eeeaa509a75a567cffb59148933a118de8df3f2c
Andres Cordero <an.cornmo@gmail.com>
2025-02-26 23:38:22 -0600

    Adding elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import NavbarComponent from './components/NavbarComponent'
 
 function App() {
 
   return (
     <>
-
+      <NavbarComponent/>
     </>
   )
 }
---
+++ src/components/NavbarComponent.jsx
@@ -0,0 +1,15 @@
+
+const NavbarComponent = () => {
+    return(
+        <nav>
+            <a href="">Eventos</a>
+            <a href="">Zona Biker</a>
+            <a href="">Galeria</a>
+            <a href="">Contacto</a>
+            
+        </nav>
+    )
+}
+
+
+export default NavbarComponent
