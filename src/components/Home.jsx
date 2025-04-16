0cd184180b62f26391d585b7be9d6db18361e791
Andres Cordero <an.cornmo@gmail.com>
2025-04-15 18:00:21 -0600

    footer added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
                     }}
                 >
                     <path
-                        d="M0,158.755s63.9,52.163,179.472,50.736c121.494-1.5,185.839-49.738,305.984-49.733,109.21,0,181.491,51.733,300.537,50.233,123.941-1.562,225.214-50.126,"
+                        d="M0,158.755s63.9,52.163,179.472,50.736c121.494-1.5,185.839-49.738,305.984-49.733,109.21,0,181.491,51.733,300.537,50.233,123.941-1.562,225.214-50.126,390.43-50.374,123.821-.185,353.982,58.374,458.976,56.373,217.907-4.153,284.6-57.236,284.6-57.236V351.03H0V158.755Z"
                         style={{ fill: '#000' }}
                     ></path>
                 </svg>
@@ -139,7 +139,7 @@ const Home = () => {
 
 
                 <section
-                    className="text-white text-center mt-3 p-md-3 position-relative rounded d-block mx-3 mx-md-5"
+                    className="text-white text-center mt-3 p-md-3 position-relative rounded d-block mx-3 mx-md-5 my-md-5"
                     style={{
                         background: 'linear-gradient(135deg,rgb(0, 0, 0),rgba(46, 46, 46, 0.73))',
                     }}
@@ -204,7 +204,7 @@ const Home = () => {
                                 <img
                                     src={Cabin}
                                     alt="Zona Motocross"
-                                    className="w-100 h-100"
+                                    className="w-100 h-50"
                                     style={{ objectFit: 'contain' }}
                                 />
                             </div>
